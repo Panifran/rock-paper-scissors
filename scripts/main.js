838b4ee2688a4370f83b91447165571d10cac624
Javier Paniagua <panifran2000@gmail.com>
2020-01-28 21:01:58 +0100

    5 Rounds structure

--- scripts/main.js
+++ scripts/main.js
@@ -5,44 +5,153 @@
 // const playerSelection = 'rock'
 // const computerSelection = computerPlay()
 // console.log(playRound(playerSelection, computerSelection))
-
 let counterPlayer = 0;
 let counterComputer = 0;
 
+// First Round
 
-function playRound1(playerSelection, computerSelection) {
+function playRound (playerSelection, computerSelection) {
+ 
   if (playerSelection == "scissors" && computerSelection == "paper") {
+    alert('Player wins');
     return counterPlayer++;
-  } else if (playerSelection == "scissors" && computerSelection == "rock") {
+  } else if (playerSelection == "scissors" && computerSelection == 'rock' ) {
+    alert('Computer wins');
     return counterComputer++;
   }
   else if (playerSelection == "paper" && computerSelection == "rock") {
+    alert('Player wins');
     return counterPlayer++;
 }
 else if (playerSelection == "paper" && computerSelection == "scissors") {
+    alert('Computer wins');
     return counterComputer++;
 }
 else if (playerSelection == "rock" && computerSelection == "scissors") {
+  alert('Player wins');
     return counterPlayer++;
 }
 else if (playerSelection == "rock" && computerSelection == "paper") {
+  alert('Computer wins');
     return counterComputer++;
 }
+else {
+  alert('Draw')
 }
-// const playerSelection = 'rock';
-// const computerSelection = 'paper';
-playerSelection = "scissors";
-computerSelection = Math.random();
+}
 
-	if (computerSelection < 0.34) {
+playerSelection = prompt('rock, paper, scissors');
+computerSelection = Math.random()
+	if (Math.random() <= 0.33) {
 		computerSelection = "rock";
-	} else if (computerSelection < 0.67) {
-		computerSelection = "paper";
+	} else if (Math.random() > 0.33 && Math.random() <= 0.66) {
+		computerSelection = 'scissors';
 	} else {
-		computerSelection = "scissors";
-    }
+		computerSelection = "paper";
+	}
+  
 
-console.log(playRound1(playerSelection, computerSelection));
+playRound(playerSelection, computerSelection);
 
+console.log(playerSelection);
 console.log(computerSelection);
-console.log(`Player: ${counterPlayer} Computer: ${counterComputer}`);
+console.log(counterPlayer);
+console.log(counterComputer);
+
+// Second Round
+
+
+playerSelection = prompt('rock, paper, scissors');
+computerSelection = Math.random()
+	if (Math.random() <= 0.33) {
+		computerSelection = "rock";
+	} else if (Math.random() > 0.33 && Math.random() <= 0.66) {
+		computerSelection = 'scissors';
+	} else {
+		computerSelection = "paper";
+	}
+  
+
+playRound(playerSelection, computerSelection);
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(counterPlayer);
+console.log(counterComputer);
+
+// Third Round
+
+
+playerSelection = prompt('rock, paper, scissors');
+computerSelection = Math.random()
+	if (Math.random() <= 0.33) {
+		computerSelection = "rock";
+	} else if (Math.random() > 0.33 && Math.random() <= 0.66) {
+		computerSelection = 'scissors';
+	} else {
+		computerSelection = "paper";
+	}
+  
+
+playRound(playerSelection, computerSelection);
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(counterPlayer);
+console.log(counterComputer);
+
+// Fourth Round
+
+
+playerSelection = prompt('rock, paper, scissors');
+computerSelection = Math.random()
+	if (Math.random() <= 0.33) {
+		computerSelection = "rock";
+	} else if (Math.random() > 0.33 && Math.random() <= 0.66) {
+		computerSelection = 'scissors';
+	} else {
+		computerSelection = "paper";
+	}
+  
+
+playRound(playerSelection, computerSelection);
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(counterPlayer);
+console.log(counterComputer);
+
+// Fifth Round
+
+
+playerSelection = prompt('rock, paper, scissors');
+computerSelection = Math.random()
+	if (Math.random() <= 0.33) {
+		computerSelection = "rock";
+	} else if (Math.random() > 0.33 && Math.random() <= 0.66) {
+		computerSelection = 'scissors';
+	} else {
+		computerSelection = "paper";
+	}
+  
+
+playRound(playerSelection, computerSelection);
+
+console.log(playerSelection);
+console.log(computerSelection);
+console.log(counterPlayer);
+console.log(counterComputer);
+
+
+
+function game(counterPlayer, counterComputer) {
+  if (counterPlayer > counterComputer)  {
+  alert('Player wins the game');
+} else if (counterPlayer < counterComputer) {
+  alert('Computer wins the game') 
+} else {
+  alert('Game draw')
+}
+}
+console.log(game())
+
